426a80c20e9d0db543be4c6431313020beecde86
Mahsa Zamanifard <103516470+Mahsa-Zf@users.noreply.github.com>
2022-09-12 03:29:33 +0430

    Update CovidVizCodes.sql

--- CovidVizCodes.sql
+++ CovidVizCodes.sql
@@ -1,6 +1,6 @@
 --1st Visualization
 
---Using Cntrl+Shift+C we copy rows and headers 
+
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From CovidProject..CovidDeath
 --Where location like '%states%'
